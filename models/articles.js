2ac5636810d46f7b47067d6af4ec8b415b50a855
MaxxABillion <afpilot2020@gmail.com>
2020-02-29 21:42:16 -0500

    update

--- models/articles.js
+++ models/articles.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const articlesSchema = new mongoose.Schema({
+var articlesSchema = new mongoose.Schema({
     author: String, content: String, description: String, publishedAt: Date, source_id: String, summarization: String, title: String, url: String, urlToImage: String}, { collection: 'Articles'});
 
-module.exports = mongoose.model('articles', articlesSchema)
+module.exports = mongoose.model('Articles', articlesSchema);
