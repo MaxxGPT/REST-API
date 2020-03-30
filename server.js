b64b9808b8f87f98e662e7ff9cd50b3a19adda96
MaxxABillion <afpilot2020@gmail.com>
2020-03-30 07:26:46 -0400

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express') 
 const app = express()
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true , useUnifiedTopology: true })
 const db = mongoose.connection
@@ -13,5 +14,33 @@ app.use(express.json())
 
 const articlesRouter = require('./routes/articles')
 app.use('/articles', articlesRouter)
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register',(req, res) => {
+    try{
+
+    } catch {
+        
+    }
+
+})
 
 app.listen(3000, () => console.log('Server Started'))
