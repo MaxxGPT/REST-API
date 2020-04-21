1c314c389b377c8d89b35b0ae0906ec73c7d68db
MaxxABillion <afpilot2020@gmail.com>
2020-04-21 12:21:42 -0400

    removed bullets

--- app/src/components/Sidebar.js
+++ app/src/components/Sidebar.js
@@ -5,28 +5,30 @@ import "../Styles/sidebar.css";
 export default class Sidebar extends Component {
   render() {
     return (
-      <nav role="navigation" class="sidebar-nav" id="Sidebar-scroll">
-          <div class="sidebar-nav-group NavGroup-shown">
-            <ul class="sidebar-nav-times loaded"> 
-              <li class>
-                <a class="sidebar-nav-item hoverable selected" href="/Developers/#intro">Introduction</a>
-              </li>
-              <li class>
-                <a class="sidebar-nav-item hoverable selected" href="/Developers/#authentication">Authentication</a> 
-              </li>
-              <li class>
-                <a class="sidebar-nav-item hoverable selected" href="/Developers/#errors">Errors</a>
-              </li>
-              <li class>
-                <a class="sidebar-nav-item hoverable selected" href="/Developers/#endpoints">Endpoints</a>
-              </li>
-              <li class> 
-                <a class="sidebar-nav-item hoverable selected" href="/Developers#libraries">Libraries</a>
-              </li>
-                     
-            </ul>
-          </div>
-      </nav>
+      <div id="sidebar">
+        <nav role="navigation" class="sidebar-nav" id="Sidebar-scroll">
+            <div class="sidebar-nav-group NavGroup-shown">
+              <ul class="sidebar-nav-times loaded"> 
+                <li class>
+                  <a class="sidebar-nav-item hoverable selected" href="/Developers/#intro">Introduction</a>
+                </li>
+                <li class>
+                  <a class="sidebar-nav-item hoverable selected" href="/Developers/#authentication">Authentication</a> 
+                </li>
+                <li class>
+                  <a class="sidebar-nav-item hoverable selected" href="/Developers/#errors">Errors</a>
+                </li>
+                <li class>
+                  <a class="sidebar-nav-item hoverable selected" href="/Developers/#endpoints">Endpoints</a>
+                </li>
+                <li class> 
+                  <a class="sidebar-nav-item hoverable selected" href="/Developers#libraries">Libraries</a>
+                </li>
+                      
+              </ul>
+            </div>
+        </nav>
+      </div>
     );
   }
 }
