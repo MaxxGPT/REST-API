46e793b3c673361eef6fb83584e094e610a7689b
Thomas Maxwell <maxxabillion@asatera.com>
2022-10-15 16:02:52 -0400

    updating stuff

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+//import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
 //import Register from './src/Register'
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
